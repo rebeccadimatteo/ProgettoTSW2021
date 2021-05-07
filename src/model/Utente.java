b952b0a679bcf113b4df277f7c18486766b06a31
rebeccadimatteo <beccadimatteoo@gmail.com>
2021-05-07 17:35:29 +0200

    Aggiornamento

---
+++ src/model/Utente.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Utente {
+	
+
+	private String id;
+	private String nome;
+	private String cognome;
+	private String cf;
+	private String email;
+	private String cellulare ;
+	private String tipologia;
+	private int numeroordini;
+	private String pswd;
+	
+
+}
